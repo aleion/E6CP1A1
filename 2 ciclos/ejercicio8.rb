9fc6331d51703f721a32d1aa6da7d42a7efd4e72
aleion <alejandro.ion.carvajal@gmail.com>
2018-11-26 21:32:31 -0300

    ejercicio 8 listo

--- 2 ciclos/ejercicio8.rb
+++ 2 ciclos/ejercicio8.rb
@@ -1,13 +1,13 @@
 # Generar -utilizando un ciclo iterativo- un string con la siguiente estructura:
 # a = '1impar 2par 3impar 4par 5impar 6par 7impar 8par 9impar 10par'
 
-a = ''
+
 10.times do |i|
     i += 1
 	if i.even?
-        puts a + 'par'
+        print i.to_s+ 'par '
       else
-        puts a + 'impar'
+        print i.to_s + 'impar '
+  end
 end
-
-puts a
+puts
